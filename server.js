714d8cb9607f33c86af8a6c683d0a44470412a06
skay97 <salmankarimzindani@hotmail.com>
2019-04-16 20:31:15 -0500

    update

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3001;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB-------Need to fix this
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
